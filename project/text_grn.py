2de45f931db7f4342890ac61efea767bb1a80704
VaibhavBiyawala <vb.vaibhavbiyawala08@gmail.com>
2025-02-05 13:53:05 +0530

    Add text generation script and dependencies

---
+++ project/text_grn.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from transformers import pipeline
+
+
+load_dotenv()
+# openai_key = os.getenv("OPENAI_API_KEY")
+huggingface_key = os.getenv("HUGGINGFACE_API_KEY")
+# groq_key = os.getenv("GROQ_API_KEY")
+
+# Load text generation model
+generator = pipeline("text-generation", model="gpt2")
+
+# Generate text
+prompt = input("Enter a prompt: ")
+response = generator(prompt, max_length=50)
+print(response[0]['generated_text'])
